615deb6c65a4493e85fa647de8d59c206ab0c134
tywalch <tywalch@gmail.com>
2023-01-16 11:33:39 -0500

    Adds tests for empty sets

--- test/ts_connected.update.spec.ts
+++ test/ts_connected.update.spec.ts
@@ -670,7 +670,188 @@ describe("Update Item", () => {
             });
         });
 
+        describe('Set attributes and empty sets', () => {
+            const tasks = new Entity(
+                {
+                    model: {
+                        entity: "tasks",
+                        version: "1",
+                        service: "taskapp",
+                    },
+                    attributes: {
+                        id: {
+                            type: "string",
+                            required: true,
+                        },
+                        str: {
+                            type: 'string'
+                        },
+                        set: {
+                            type: 'set',
+                            items: 'string'
+                        },
+                        map: {
+                            type: 'map',
+                            properties: {
+                                set: {
+                                    type: 'set',
+                                    items: 'string',
+                                },
+                                map: {
+                                    type: 'map',
+                                    properties: {
+                                        set: {
+                                            type: 'set',
+                                            items: 'string',
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    indexes: {
+                        projects: {
+                            pk: {
+                                field: "pk",
+                                composite: ["id"],
+                            },
+                            sk: {
+                                field: "sk",
+                                // create composite keys for partial sort key queries
+                                composite: [],
+                            },
+                        },
+                    },
+                },
+                { table, client }
+            );
+            it('should not try perform a set operation an empty Set attribute', async () => {
+                const id = uuid();
+                let params1: any;
+                await tasks.create({ id, map: { map: {} }}).go();
+                await tasks
+                    .update({ id })
+                    .data((attr, op) => {
+                        op.set(attr.set, []);
+                        op.set(attr.map.set, []);
+                        op.set(attr.map.map.set, []);
+                    })
+                    .go({
+                        logger: (event) => {
+                            if (event.type === 'query') {
+                                params1 = event.params;
+                            }
+                        }
+                    });
+
+                expect(params1).to.deep.equal({
+                    "UpdateExpression": "SET #id = :id_u0, #__edb_e__ = :__edb_e___u0, #__edb_v__ = :__edb_v___u0",
+                    "ExpressionAttributeNames": {
+                        "#id": "id",
+                        "#__edb_e__": "__edb_e__",
+                        "#__edb_v__": "__edb_v__"
+                    },
+                    "ExpressionAttributeValues": {
+                        ":id_u0": id,
+                        ":__edb_e___u0": "tasks",
+                        ":__edb_v___u0": "1"
+                    },
+                    "TableName": "electro",
+                    "Key": {
+                        "pk": `$taskapp#id_${id}`,
+                        "sk": "$tasks_1"
+                    }
+                });
+
+                // should perform set just fine
+                const id2 = uuid();
+                let params2: any;
+                await tasks.create({ id: id2, map: { map: {} }}).go();
+                await tasks
+                    .update({ id: id2 })
+                    .data((attr, op) => {
+                        op.set(attr.set, ['set1']);
+                        op.set(attr.map.set, ['set2']);
+                        op.set(attr.map.map.set, ['set3']);
+                    })
+                    .go({
+                        logger: (event) => {
+                            if (event.type === 'query') {
+                                params2 = event.params;
+                            }
+                        }
+                    });
+                expect(params2.UpdateExpression).to.equal("SET #set = :set_u0, #map.#set = :set_u1, #map.#map.#set = :set_u2, #id = :id_u0, #__edb_e__ = :__edb_e___u0, #__edb_v__ = :__edb_v___u0");
+                expect(params2.ExpressionAttributeValues[":set_u0"].values).to.deep.equal(["set1"]);
+                expect(params2.ExpressionAttributeValues[":set_u1"].values).to.deep.equal(["set2"]);
+                expect(params2.ExpressionAttributeValues[":set_u2"].values).to.deep.equal(["set3"]);
+            });
+
+            it('should not try perform an add operation an empty Set attribute', async () => {
+                const id = uuid();
+                let params1: any;
+                await tasks.create({ id, map: { map: {} }}).go();
+                await tasks
+                    .update({ id })
+                    .data((attr, op) => {
+                        op.add(attr.set, []);
+                        op.add(attr.map.set, []);
+                        op.add(attr.map.map.set, []);
+                    })
+                    .go({
+                        logger: (event) => {
+                            if (event.type === 'query') {
+                                params1 = event.params;
+                            }
+                        }
+                    });
+
+                expect(params1).to.deep.equal({
+                    "UpdateExpression": "SET #id = :id_u0, #__edb_e__ = :__edb_e___u0, #__edb_v__ = :__edb_v___u0",
+                    "ExpressionAttributeNames": {
+                        "#id": "id",
+                        "#__edb_e__": "__edb_e__",
+                        "#__edb_v__": "__edb_v__"
+                    },
+                    "ExpressionAttributeValues": {
+                        ":id_u0": id,
+                        ":__edb_e___u0": "tasks",
+                        ":__edb_v___u0": "1"
+                    },
+                    "TableName": "electro",
+                    "Key": {
+                        "pk": `$taskapp#id_${id}`,
+                        "sk": "$tasks_1"
+                    }
+                });
+
+                // should perform add just fine
+                const id2 = uuid();
+                let params2: any;
+                await tasks.create({ id: id2, map: { map: {} }}).go();
+                await tasks
+                    .update({ id: id2 })
+                    .data((attr, op) => {
+                        op.add(attr.set, ['set1']);
+                        op.add(attr.map.set, ['set2']);
+                        op.add(attr.map.map.set, ['set3']);
+                    })
+                    .go({
+                        logger: (event) => {
+                            if (event.type === 'query') {
+                                params2 = event.params;
+                            }
+                        }
+                    });
+                expect(params2.UpdateExpression).to.equal("SET #id = :id_u0, #__edb_e__ = :__edb_e___u0, #__edb_v__ = :__edb_v___u0 ADD #set :set_u0, #map.#set :set_u1, #map.#map.#set :set_u2");
+                expect(params2.ExpressionAttributeValues[":set_u0"].values).to.deep.equal(["set1"]);
+                expect(params2.ExpressionAttributeValues[":set_u1"].values).to.deep.equal(["set2"]);
+                expect(params2.ExpressionAttributeValues[":set_u2"].values).to.deep.equal(["set3"]);
+            });
+        });
+
         describe('Map Attributes and empty objects', () => {
+
             it('should return an empty object with a Map Attribute when one is set via a static default', async () => {
                 const entityWithDefault = new Entity({
                     model: {
